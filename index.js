a8a555388d6b588cf766e329d4cb349efbe841c6
Vincnet irungu <vrugundu001@gmail.com>
2023-10-01 15:33:32 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 =31;
+const multiply= num1*num2;
 
-// Write your code here
+const random= Math.floor(Math.random() * 10) + 1;
+
+const num3 = 22;
+const num4 = 6;
+const mod=4;
+num3%num4===4;
+
+
+const max= Math.max(1,20)
